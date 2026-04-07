b7d14439fa034228c8a8fcd4935bd0cdf4d3bac9
VarshaShetty28 <varshashetty551@gmail.com>
2026-04-07 23:32:53 +0530

    Added awards page UI with sidebar and layout

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,7 +1,19 @@
-
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './shared/layout/layout.component';
+import { AwardsComponent } from './features/admin/awards/awards.component';
 import { LoginComponent } from './features/auth/login/login.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent }
+
+  { path: '', component: LoginComponent },
+
+  {
+    path: 'admin',
+    component: LayoutComponent, // WRAPPER
+    children: [
+      { path: '', component: AwardsComponent }, // temporary
+      { path: 'awards', component: AwardsComponent }
+    ]
+  }
+
 ];
